9bcd480a54de5a83767f61d11a92d74bb14e81ce
Amulya Meka <amulyameka38@gmail.com>
2026-05-13 16:26:32 +0530

    Update powerpc64le-options.mk

--- src/runtime-rs/arch/powerpc64le-options.mk
+++ src/runtime-rs/arch/powerpc64le-options.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# ppc64le
 MACHINETYPE := pseries
 KERNELPARAMS := cgroup_no_v1=all systemd.unified_cgroup_hierarchy=1
 MACHINEACCELERATORS := "cap-cfpc=broken,cap-sbbc=broken,cap-ibs=broken,cap-large-decr=off,cap-ccf-assist=off"
