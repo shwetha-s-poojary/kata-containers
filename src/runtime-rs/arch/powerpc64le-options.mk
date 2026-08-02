129001130697cb2ad3b251781a8369398483ea28
shwetha-s-poojary <shwetha.s-poojary@ibm.com>
2026-08-02 15:22:37 +0530

    runtime-rs support plus containerd only test
    Signed-off-by: shwetha-s-poojary <shwetha.s-poojary@ibm.com>

--- src/runtime-rs/arch/powerpc64le-options.mk
+++ src/runtime-rs/arch/powerpc64le-options.mk
@@ -6,7 +6,8 @@
 
 MACHINETYPE := pseries
 KERNELPARAMS := cgroup_no_v1=all systemd.unified_cgroup_hierarchy=1
-MACHINEACCELERATORS := "cap-cfpc=broken,cap-sbbc=broken,cap-ibs=broken,cap-large-decr=off,cap-ccf-assist=off"
+# https://www.qemu.org/docs/master/specs/ppc-spapr-xive.html#xive-for-spapr-pseries-machines
+MACHINEACCELERATORS := "ic-mode=xics,cap-cfpc=broken,cap-sbbc=broken,cap-ibs=broken,cap-large-decr=off,cap-ccf-assist=off"
 CPUFEATURES :=
 
 QEMUCMD := qemu-system-ppc64
